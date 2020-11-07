5f5d19b9b5b59fcba9699bf81b0884025fbae2d5
Ugur Cagatay Tuzun <cagatay.tuzun@alo-tech.com>
2020-11-08 00:26:09 +0300

    first commit

---
+++ mars.py
@@ -0,0 +1,18 @@
+from random import choice
+
+import requests
+import time
+
+mars_rover_url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
+
+
+def get_mars_photo(sol):
+    time.sleep(5)
+    print("****")
+    print(sol)
+    params = { 'sol': sol, 'api_key': 'DEMO_KEY' }
+    response = requests.get(mars_rover_url, params).json()
+    photos = response['photos']
+
+    image = choice(photos)['img_src']
+    print(image)
